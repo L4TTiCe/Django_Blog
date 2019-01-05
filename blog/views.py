cc3c463af15c74b60da0fcdec7a2de693e42fd51
Dheshan M <dheshan.mail@gmail.com>
2019-01-05 13:22:15 +0530

    [MINOR] Added in Dummy data to view / Test     * Made Title in html files Dynamic
    Signed-off-by: Dheshan M <dheshan.mail@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'PyCharm',
+        'title': 'Django',
+        'content': 'Django is the award-winning leader of Python web frameworks and PyCharm has long supported it. '
+                   'Running, debugging, navigating, working productively... PyCharm has you covered for Django.',
+        'date_posted': 'January 4, 2019'
+    },
+    {
+        'author': 'PyCharm',
+        'title': 'Flask',
+        'content': 'The fast-growing Flask microframework has strong and growing PyCharm support: templates, '
+                   'navigation, completion, and more.',
+        'date_posted': 'January 5, 2019'
+    }
+]
 
 # Create your views here.
 def landing(response):
-    return render(response, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(response, 'blog/home.html', context)
     # return HttpResponse('<h1> Blog Landing </h1>')
 
 
 def about(response):
-    return render(response, 'blog/about.html')
+    return render(response, 'blog/about.html', { 'title': 'About' })
     # return HttpResponse('<h1> About Placehonder </h1>')
